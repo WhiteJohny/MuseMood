b924f72dbfb0509c48a6c3fb72175245b7d9e8c7
Egor Kiselev <eakiselev@yandex.ru>
2025-04-14 23:37:23 +0300

    feat: a-z dataset

--- dataset/preprocessing_dataset.py
+++ dataset/preprocessing_dataset.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import os
+import shutil
+
+from audio_parsers.download_audio import download
+from audio_parsers.yt_link import download_audio_as_wav
+from audio_converter.make_dataset import make_dataset
 
 
 LINK_PATTERN = 'https://www.youtube.com/watch?v='
@@ -22,14 +27,14 @@ def create_class_labels(data: pd.DataFrame) -> pd.DataFrame:
     return pd.DataFrame(labels, columns=['index', 'display_name', 'mid'])
 
 
-def fix_unprocessed_dataset(path: str, dir_name: str) -> None:
+def fix_unprocessed_dataset(path: str, name_dir: str) -> None:
     with open(path, 'r') as f:
         text = f.read().replace(', ', ',')
 
-    if not os.path.isdir(dir_name):
-        os.mkdir(dir_name)
+    if not os.path.isdir(name_dir):
+        os.mkdir(name_dir)
 
-    with open(dir_name + '/edited_' + path[path.index('/') + 1:], 'w') as f:
+    with open(name_dir + '/edited_' + path[path.index('/') + 1:], 'w') as f:
         f.write(text)
 
 
@@ -76,23 +81,30 @@ def create_preprocessing_dataset(data: pd.DataFrame, labels: pd.DataFrame) -> pd
     )
 
 
-sentiment_labels = pd.read_csv('raw_data/class_labels_indices.csv')
+file_path = 'raw_data/class_labels_indices.csv'
+sentiment_labels = pd.read_csv(file_path)
 sentiment_labels = create_class_labels(sentiment_labels)
 
-fix_unprocessed_dataset('raw_data/unbalanced_train_segments.csv', 'edited_data')
-fix_unprocessed_dataset('raw_data/balanced_train_segments.csv', 'edited_data')
-fix_unprocessed_dataset('raw_data/eval_segments.csv', 'edited_data')
+dir_name = 'edited_data'
+fix_unprocessed_dataset('raw_data/unbalanced_train_segments.csv', dir_name)
+fix_unprocessed_dataset('raw_data/balanced_train_segments.csv', dir_name)
+fix_unprocessed_dataset('raw_data/eval_segments.csv', dir_name)
 
+file_path = 'edited_data/edited_unbalanced_train_segments.csv'
 unprocessed_unbalanced_dataset = pd.read_csv(
-    'edited_data/edited_unbalanced_train_segments.csv',
+    file_path,
     on_bad_lines='skip'
 )
+
+file_path = 'edited_data/edited_balanced_train_segments.csv'
 unprocessed_balanced_dataset = pd.read_csv(
-    'edited_data/edited_balanced_train_segments.csv',
+    file_path,
     on_bad_lines='skip'
 )
+
+file_path = 'edited_data/edited_eval_segments.csv'
 unprocessed_eval_dataset = pd.read_csv(
-    'edited_data/edited_eval_segments.csv',
+    file_path,
     on_bad_lines='skip'
 )
 
@@ -104,21 +116,59 @@ dir_name = 'preprocessing_data'
 if not os.path.isdir(dir_name):
     os.mkdir(dir_name)
 
+file_path = dir_name + '/preprocessing_unbalanced_dataset.csv'
 preprocessing_unbalanced_dataset.to_csv(
-    dir_name + '/preprocessing_unbalanced_dataset.csv',
+    file_path,
     sep=',',
     index=False,
     encoding='utf-8'
 )
+
+file_path = dir_name + '/preprocessing_balanced_dataset.csv'
 preprocessing_balanced_dataset.to_csv(
-    dir_name + '/preprocessing_balanced_dataset.csv',
+    file_path,
     sep=',',
     index=False,
     encoding='utf-8'
 )
+
+file_path = dir_name + '/preprocessing_eval_dataset.csv'
 preprocessing_eval_dataset.to_csv(
-    dir_name + '/preprocessing_eval_dataset.csv',
+    file_path,
     sep=',',
     index=False,
     encoding='utf-8'
 )
+
+preprocessed_unbalanced_dataset = pd.read_csv('preprocessing_data/preprocessing_unbalanced_dataset.csv')
+preprocessed_balanced_dataset = pd.read_csv('preprocessing_data/preprocessing_balanced_dataset.csv')
+preprocessed_eval_dataset = pd.read_csv('preprocessing_data/preprocessing_eval_dataset.csv')
+
+
+dir_name = 'unbalanced_audio'
+if os.path.isdir(dir_name):
+    shutil.rmtree(dir_name)
+download(download_audio_as_wav, preprocessed_unbalanced_dataset, dir_name)
+make_dataset(dir_name, 'unbalanced_dataset.csv', preprocessed_unbalanced_dataset)
+
+dir_name = 'balanced_audio'
+if os.path.isdir(dir_name):
+    shutil.rmtree(dir_name)
+download(download_audio_as_wav, preprocessed_balanced_dataset, dir_name)
+make_dataset(dir_name, 'balanced_dataset.csv', preprocessed_balanced_dataset)
+
+dir_name = 'eval_audio'
+if os.path.isdir(dir_name):
+    shutil.rmtree(dir_name)
+download(download_audio_as_wav, preprocessed_eval_dataset, 'eval_audio')
+make_dataset(dir_name, 'eval_dataset.csv', preprocessed_eval_dataset)
+
+# Вектор из строки в np.ndarray (пример)
+# df = pd.read_csv("data.csv")
+# features_vector = np.array(eval(df["features"][0]))
+
+shutil.rmtree('edited_data')
+shutil.rmtree('preprocessing_data')
+shutil.rmtree('unbalanced_audio')
+shutil.rmtree('balanced_audio')
+shutil.rmtree('eval_audio')
